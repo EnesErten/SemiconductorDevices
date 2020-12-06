fe6bd20f3ee13937878fd903cc9ffa38eb5255ae
EnesErten <62370707+EnesErten@users.noreply.github.com>
2020-12-06 15:06:07 +0300

    Add files via upload

---
+++ fullwvbridgerect.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.arange(0, 1e-2, 1e-5)
+ac_signal = 10 * np.sin(2 * np.pi * 1e3 * t)
+output = []
+
+for i in ac_signal:
+    if abs(i) >= 1.4:
+        output.append(abs(i) - 1.4)
+
+    else:
+        output.append(0)
+
+plt.plot(t, output, 'r', label="Output Voltage")
+plt.plot(t, ac_signal, 'b', label="Input Voltage")
+plt.grid()
+plt.legend()
+plt.show()
